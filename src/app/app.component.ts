322d8fd3b05bdef2f318569e9b497be1abc86679
bramjanssens <bram.janssens@infosupport.com>
2020-09-01 10:04:49 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  color = 'red';
+  show = true;
+  name: string;
+  filter: string;
+  myFormGroup: FormGroup;
+  searchControl: FormControl;
+
+  readonly users = [
+    'Sander',
+    'Bram',
+    'Ruben',
+    'Jochem',
+    'Jonas',
+    'Glenn'
+  ];
+  filteredUsers = [...this.users];
+
+  constructor(private fb: FormBuilder) { // fb is injected
+    /* this.myFormGroup = new FormGroup({
+      name: new FormControl('', [Validators.required])
+    }); */
+
+    // or shorter:
+    this.myFormGroup = this.fb.group({
+      name: ['', [Validators.required]]
+    });
+
+    this.searchControl = this.fb.control('');
+    this.searchControl.valueChanges.subscribe(newFilter => {
+      this.doFiltering(newFilter);
+    });
+  }
+
+  save() {
+    console.log(this.myFormGroup.value);
+  }
+
+  flip() {
+    this.show = !this.show;
+  }
+
+  onChange(e: InputEvent): void {
+    const input = e.target as HTMLInputElement;
+    const inVal = input.value;
+    this.name = inVal;
+  }
+
+  doFiltering(value: string) {
+    this.filter = value;
+    this.filteredUsers = this.users.filter(name => name.indexOf(value) >= 0);
+  }
+
 }
