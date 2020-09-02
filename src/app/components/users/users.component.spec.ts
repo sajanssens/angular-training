981202d53b9011916151234db771f38ba56eeb9e
bramjanssens <bram.janssens@infosupport.com>
2020-09-02 11:08:00 +0200

    IntTest added

--- src/app/components/users/users.component.spec.ts
+++ src/app/components/users/users.component.spec.ts
@@ -1,16 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from 'src/app/app.component';
 import { UsersComponent } from './users.component';
 
-describe('UsersComponent', () => {
+fdescribe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      declarations: [UsersComponent, AppComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +23,39 @@ describe('UsersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});
+
+fdescribe('when parent has 6 users', () => {
+  let component: UsersComponent;
+  let fixture: ComponentFixture<UsersComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [UsersComponent],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersComponent);
+    component = fixture.componentInstance;
+    component.users = [
+      { name: 'Sander' },
+      { name: 'Bram' },
+      { name: 'Ruben' },
+      { name: 'Jochem' },
+      { name: 'Jonas' },
+      { name: 'Glenn' }
+    ];
+    fixture.detectChanges();
+  });
+
+  it('should render six users', async(() => { // let op async!
+    fixture.whenStable().then(() => {
+      expect(fixture.nativeElement.querySelectorAll('tr').length).toBe(6);
+    });
+  }));
 });
