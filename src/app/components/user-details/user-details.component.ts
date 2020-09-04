e48325693bfe3c4a523bd2292508e02258f024f5
bramjanssens <bram.janssens@infosupport.com>
2020-09-04 11:27:06 +0200

    Improved details.

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
-import { map, shareReplay, startWith, switchMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from 'src/app/domain/user';
 import { UserService } from 'src/app/services/user.service';
 
@@ -11,30 +11,17 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
-  updateHappened$ = new Subject<undefined>();
-  
-  user$: Observable<User> = this.updateHappened$.pipe(
-    startWith(undefined),
-    switchMap(() => this.us.get(8)),
-    shareReplay(1)
-  );
 
-  id: number = 0;
-  name: string = 'x';
-
-
+  user$: Observable<User>;
 
   constructor(private route: ActivatedRoute, private us: UserService) { }
 
   ngOnInit(): void {
     this.route.params
       .pipe(map(params => params['id']))
-      // .subscribe(id => this.us.get(id).subscribe(u => this.user = u));
       .subscribe(id => this.user$ = this.us.get(id));
-    
-    // not working: try with updateHappened...
-    this.user$.pipe(map(u => this.id = u.id));
-    this.user$.pipe(map(u => this.name = u.name));
   }
 
 }
+
+
